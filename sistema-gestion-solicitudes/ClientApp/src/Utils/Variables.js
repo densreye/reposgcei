344e443750de5d74380b10e68a0f01462418528c
Wesley Briones <wesgbrio@espol.edu.ec>
2024-02-09 22:18:02 -0500

    cambio de la ruta de acceso al cas

--- sistema-gestion-solicitudes/ClientApp/src/Utils/Variables.js
+++ sistema-gestion-solicitudes/ClientApp/src/Utils/Variables.js
@@ -1,6 +1,9 @@
 ﻿
 export const API_URL = "https://localhost:7191/api";
-export const authUrl = "http://auth.test.espol.edu.ec/login?service=http://localhost:44382/login.aspx";
+
+//export const authUrl = "http://auth.test.espol.edu.ec/login?service=http://localhost:7191/login.aspx";
+export const authUrl = "https://auth.test.espol.edu.ec/login?service=https://localhost:44448/Welcome";
+
 export const urlFront = "https://localhost:44448/";
 
 export const MessageEmail = "Me gustaria concretar una reunión de forma presencial para indagar más detalles acerca del proyecto a revisar";
